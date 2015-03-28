20febc4bc6602efd7301ee82ab9b4b55b7c34744
Chengzhen Yang <czyang.jessie@gmail.com>
2015-03-28 16:43:21 +0800

    update IM

---
+++ Sys-CRM/crmSystem.java
@@ -0,0 +1,263 @@
+/*
+ * =================================================================
+ * Copyright (c) 2001-2003 TIBCO Software Inc.
+ * All rights reserved.
+ * For more information, please contact:
+ * TIBCO Software Inc., Palo Alto, California, USA
+ *
+ * $RCSfile: tibjmsMsgConsumer.java,v $
+ * $Revision: 1.7 $
+ * $Date: 2004/02/06 00:05:02 $
+ * =================================================================
+ */
+
+/*
+ * This is a simple sample of a basic tibjmsMsgConsumer.
+ *
+ * This sampe subscribes to specified destination and
+ * receives and prints all received messages.
+ *
+ * Notice that the specified destination should exist in your configuration
+ * or your topics/queues configuration file should allow
+ * creation of the specified destination.
+ *
+ * If this sample is used to receive messages published by
+ * tibjmsMsgProducer sample, it must be started prior
+ * to running the tibjmsMsgProducer sample.
+ *
+ * Usage:  java tibjmsMsgConsumer [options]
+ *
+ *    where options are:
+ *
+ *      -server     Server URL.
+ *                  If not specified this sample assumes a
+ *                  serverUrl of "tcp://localhost:7222"
+ *
+ *      -user       User name. Default is null.
+ *      -password   User password. Default is null.
+ *      -topic      Topic name. Default is "topic.sample"
+ *      -queue      Queue name. No default
+ *
+ *
+ */
+
+import javax.jms.*;
+
+public class crmSystem
+        implements ExceptionListener {
+    /*-----------------------------------------------------------------------
+     * Parameters
+     *----------------------------------------------------------------------*/
+
+    String serverUrl = null;
+    String userName = null;
+    String password = null;
+    String name = "topic.sample";
+    boolean useTopic = true;
+
+    /*-----------------------------------------------------------------------
+    * Variables
+    *----------------------------------------------------------------------*/
+    Connection connection = null;
+    Session session = null;
+    MessageConsumer msgConsumer = null;
+    Destination destination = null;
+
+    public crmSystem(String[] args) {
+        parseArgs(args);
+
+        try {
+            tibjmsUtilities.initSSLParams(serverUrl, args);
+        }
+        catch (JMSSecurityException e) {
+            System.err.println("JMSSecurityException: " + e.getMessage() + ", provider=" + e.getErrorCode());
+            e.printStackTrace();
+            System.exit(0);
+        }
+
+        /* print parameters */
+        System.err.println("\n------------------------------------------------------------------------");
+        System.err.println("Customer Relationship Management System");
+        System.err.println("------------------------------------------------------------------------");
+        System.err.println("Server....................... " + ((serverUrl != null) ? serverUrl : "localhost"));
+        System.err.println("User......................... " + ((userName != null) ? userName : "(null)"));
+        System.err.println("Destination.................. " + name);
+        System.err.println("------------------------------------------------------------------------\n");
+
+        try {
+            run();
+        } catch (JMSException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    /*-----------------------------------------------------------------------
+     * usage
+     *----------------------------------------------------------------------*/
+    void usage() {
+        System.err.println("\nUsage: tibjmsMsgConsumer [options] [ssl options]");
+        System.err.println("");
+        System.err.println("   where options are:");
+        System.err.println("");
+        System.err.println(" -server   <server URL> - EMS server URL, default is local server");
+        System.err.println(" -user     <user name>  - user name, default is null");
+        System.err.println(" -password <password>   - password, default is null");
+        System.err.println(" -topic    <topic-name> - topic name, default is \"topic.sample\"");
+        System.err.println(" -queue    <queue-name> - queue name, no default");
+        System.err.println(" -help-ssl              - help on ssl parameters\n");
+        System.exit(0);
+    }
+
+    /*-----------------------------------------------------------------------
+     * parseArgs
+     *----------------------------------------------------------------------*/
+    void parseArgs(String[] args) {
+        int i = 0;
+
+        while (i < args.length) {
+            if (args[i].compareTo("-server") == 0) {
+                if ((i + 1) >= args.length) usage();
+                serverUrl = args[i + 1];
+                i += 2;
+            } else if (args[i].compareTo("-topic") == 0) {
+                if ((i + 1) >= args.length) usage();
+                name = args[i + 1];
+                i += 2;
+            } else if (args[i].compareTo("-queue") == 0) {
+                if ((i + 1) >= args.length) usage();
+                name = args[i + 1];
+                i += 2;
+                useTopic = false;
+            } else if (args[i].compareTo("-user") == 0) {
+                if ((i + 1) >= args.length) usage();
+                userName = args[i + 1];
+                i += 2;
+            } else if (args[i].compareTo("-password") == 0) {
+                if ((i + 1) >= args.length) usage();
+                password = args[i + 1];
+                i += 2;
+            } else if (args[i].compareTo("-help") == 0) {
+                usage();
+            } else if (args[i].compareTo("-help-ssl") == 0) {
+                tibjmsUtilities.sslUsage();
+            } else if (args[i].startsWith("-ssl")) {
+                i += 2;
+            } else {
+                System.err.println("Unrecognized parameter: " + args[i]);
+                usage();
+            }
+        }
+    }
+
+
+    /*---------------------------------------------------------------------
+     * onException
+     *---------------------------------------------------------------------*/
+    public void onException(
+            JMSException e) {
+        /* print the connection exception status */
+        System.err.println("CONNECTION EXCEPTION: " + e.getMessage());
+    }
+
+    /*-----------------------------------------------------------------------
+     * run
+     *----------------------------------------------------------------------*/
+    void run()
+            throws JMSException {
+        Message msg = null;
+        String msgType = "UNKNOWN";
+
+        System.err.println("Subscribing to destination: " + name + "\n");
+
+        ConnectionFactory factory = new com.tibco.tibjms.TibjmsConnectionFactory(serverUrl);
+
+        /* create the connection */
+        connection = factory.createConnection(userName, password);
+
+        /* create the session */
+        session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+
+        /* set the exception listener */
+        connection.setExceptionListener(this);
+
+        /* create the destination */
+        if (useTopic)
+            destination = session.createTopic(name);
+        else
+            destination = session.createQueue(name);
+
+        /* create the consumer */
+        msgConsumer = session.createConsumer(destination);
+
+        /* start the connection */
+        connection.start();
+
+        while (true) {
+            /* receive the message */
+            msg = msgConsumer.receive();
+
+            if (msg == null)
+                break;
+            onMessage(msg);     //karway
+            System.err.println("Received message: " + msg);
+        }
+
+        /* close the connection */
+        connection.close();
+    }
+
+    /*-----------------------------------------------------------------------
+     * main
+     *----------------------------------------------------------------------*/
+    public static void main(String[] args) {
+        new tibjmsMsgRRConsumer(args);
+    }
+
+    // Handle the message when received.
+    public void onMessage(Message message) {
+        try {
+            if ((message instanceof TextMessage) && (message.getJMSReplyTo() != null)) {
+                TextMessage requestMessage = (TextMessage) message;
+
+                System.out.println("Received request");
+                System.out.println("\tTime:       " + System.currentTimeMillis() + " ms");
+                System.out.println("\tMessage ID: " + requestMessage.getJMSMessageID());
+                System.out.println("\tCorrel. ID: " + requestMessage.getJMSCorrelationID());
+                System.out.println("\tReply to:   " + requestMessage.getJMSReplyTo());
+                System.out.println("\tContents:   " + requestMessage.getText());
+
+                // Prepare reply message and send reply message
+                String contents = requestMessage.getText();
+                Destination replyDestination = message.getJMSReplyTo();
+                MessageProducer replyProducer = session.createProducer(replyDestination);
+
+                TextMessage replyMessage = session.createTextMessage();
+                // Hardcoded the replyMessage to for this example.
+                replyMessage.setText("Request received and queued successfully.");
+                replyMessage.setJMSCorrelationID(requestMessage.getJMSMessageID());
+                // sending reply message.
+                replyProducer.send(replyMessage);
+
+                System.out.println("Sent reply");
+                System.out.println("\tTime:       " + System.currentTimeMillis() + " ms");
+                System.out.println("\tMessage ID: " + replyMessage.getJMSMessageID());
+                System.out.println("\tCorrel. ID: " + replyMessage.getJMSCorrelationID());
+                System.out.println("\tReply to:   " + replyMessage.getJMSReplyTo());
+                System.out.println("\tContents:   " + replyMessage.getText());
+                System.out.println("\tDestination:" + replyMessage.getJMSDestination());
+            } else {
+                System.out.println("Invalid message detected");
+                System.out.println("\tType:       " + message.getClass().getName());
+                System.out.println("\tTime:       " + System.currentTimeMillis() + " ms");
+                System.out.println("\tMessage ID: " + message.getJMSMessageID());
+                System.out.println("\tCorrel. ID: " + message.getJMSCorrelationID());
+                System.out.println("\tReply to:   " + message.getJMSReplyTo());
+
+                message.setJMSCorrelationID(message.getJMSMessageID());
+            }
+        } catch (JMSException e) {
+            e.printStackTrace();
+        }
+    }
+}
